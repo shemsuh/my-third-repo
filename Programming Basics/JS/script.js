73eed8823fdabd64ab41e23800318278d3a86620
shemsuh <shemsun2@gmail.com>
2022-04-29 20:45:28 +0300

    My Programming Basic Assignment
    This is my third repo

---
+++ Programming Basics/JS/script.js
@@ -0,0 +1,14 @@
+
+console.log("Shemsu");
+let total = 0, count = 1;
+
+while (count <= 10) {
+    total += count;
+    count += 1;
+}
+
+console.log(total);
+var height=4;
+var length=3;
+var area=height*length;
+console.log(area);
